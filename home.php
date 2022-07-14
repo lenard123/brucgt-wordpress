f88b240eac6a1d218dc75a690b53cee5052b39d1
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-07-14 07:56:31 +0000

    wip

--- home.php
+++ home.php
@@ -1,4 +1,9 @@
 <?php get_header() ?>
-test
+
+<div class="bg-slate-100">
+    <div class="max-w-screen-xl h-[70vh] mx-auto container overflow-hidden">
+        <img class="w-full h-full object-cover " src="<?= get_theme_file_uri('images/banner3.png') ?>" />
+    </div>
+</div>
 
 <?php get_footer() ?>
